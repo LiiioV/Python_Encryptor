e3f96a45eb0ec0e2d16279441a4988a847b5c321
LiiioV <60844436+LiiioV@users.noreply.github.com>
2020-04-20 20:16:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -251,5 +251,4 @@ elif args.command == 'break':
     k = breaking_key(freq_counter, fin)
     copy = open(input_filename, 'r')
     for it in copy:
-        write_text(decode('caesar', k, it))
-<br/>
+        write_text(decode('caesar', k, it))  
